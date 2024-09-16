3d5685f949398f9d73e7f17c7d6d08daa7f5ae00
Psalmantha <psalmanthaallaine03@gmail.com>
2024-09-16 23:37:02 +0800

    Major revisions in UI, list order logic retained #1

--- App.tsx
+++ App.tsx
@@ -1,143 +1,168 @@
 import React, { useState, useEffect } from 'react';
 import {
-  FlatList,
   SafeAreaView,
   StatusBar,
   StyleSheet,
   Text,
-  TextInput,
   TouchableOpacity,
   View,
+  FlatList,
 } from 'react-native';
-
-import CheckBox from '@react-native-community/checkbox';
+import NewTaskModal from './NewTask';
 
 function App(): React.JSX.Element {
-  const [inputTask, setInputTask] = useState('');
-  const [tasks, setTasks] = useState<{ id: number, text: string, originalIndex: number }[]>([]);
-  const [finishedTasks, setFinishedTasks] = useState<{ id: number, text: string, originalIndex: number }[]>([]);
+  const [currentDate, setCurrentDate] = useState('');
+  const [selectedFilter, setSelectedFilter] = useState('All');
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [tasks, setTasks] = useState<{ id: number, title: string, description: string, finished: boolean, originalIndex: number, timestamp: string }[]>([]);
   const [taskIdCounter, setTaskIdCounter] = useState(0);
-  const [currentDate, setCurrentDate] = useState({ day: '', fullDate: '' });
 
   useEffect(() => {
     const date = new Date();
-  
-    // Format the full date to be like "24 Sept"
-    const options: Intl.DateTimeFormatOptions = { day: 'numeric', month: 'short' };
+    const options: Intl.DateTimeFormatOptions = { weekday: 'long', day: 'numeric', month: 'short' };
     const formattedDate = date.toLocaleDateString('en-US', options);
-  
-    // Get the day of the week (e.g., "Sat")
-    const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-    const dayOfWeek = daysOfWeek[date.getDay()];
-  
-    setCurrentDate({ day: dayOfWeek, fullDate: formattedDate });
+    setCurrentDate(formattedDate);
   }, []);
-  
-  
 
-  const addTask = () => {
-    if (inputTask.length > 0) {
-      const newId = taskIdCounter;
-      const newIndex = tasks.length;
-      setTasks([...tasks, { id: newId, text: inputTask, originalIndex: newIndex }]);
-      setTaskIdCounter(newId + 1);
-      setInputTask('');
-    }
+  const handleAddTask = (title: string, description: string) => {
+    const timestamp = new Date().toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true }) + ', ' + currentDate;
+    const newTask = {
+      id: taskIdCounter,
+      title,
+      description,
+      finished: false,
+      originalIndex: tasks.length, 
+      timestamp,
+    };
+    setTasks([...tasks, newTask]);
+    setTaskIdCounter(taskIdCounter + 1);
   };
 
   const handleCheck = (taskId: number, isChecked: boolean) => {
-    if (isChecked) {
-      const taskToMove = tasks.find(task => task.id === taskId);
-      if (taskToMove) {
-        setFinishedTasks([...finishedTasks, { ...taskToMove, originalIndex: taskToMove.originalIndex }]);
-        setTasks(tasks.filter(task => task.id !== taskId));
+    const updatedTasks = tasks.map(task => {
+      if (task.id === taskId) {
+        return { ...task, finished: isChecked };
       }
+      return task;
+    });
+    setTasks(updatedTasks);
+  };
+
+  const handleCheckAll = () => {
+    const updatedTasks = tasks.map(task => ({ ...task, finished: true }));
+    setTasks(updatedTasks);
+  };
+
+  const handleUncheckAll = () => {
+    const updatedTasks = tasks.map(task => ({ ...task, finished: false }));
+    setTasks(updatedTasks);
+  };
+
+  const renderTaskItem = ({ item }: { item: { id: number, title: string, description: string, finished: boolean, originalIndex: number, timestamp: string } }) => (
+    <View style={styles.taskItem}>
+      <TouchableOpacity onPress={() => handleCheck(item.id, !item.finished)}>
+        <View style={item.finished ? styles.finishedCircle : styles.activeCircle}></View>
+      </TouchableOpacity>
+      <View style={styles.taskDetails}>
+        <Text style={item.finished ? styles.finishedTaskTitle : styles.taskTitle}>{item.title}</Text>
+        <Text style={styles.taskDescription}>{item.description}</Text>
+        <Text style={styles.taskTimestamp}>Today, {item.timestamp}</Text>
+      </View>
+    </View>
+  );
+
+  const filterTasks = () => {
+    const activeTasks = tasks.filter(task => !task.finished);
+    const finishedTasks = tasks.filter(task => task.finished);
+    if (selectedFilter === 'All') {
+      return [...activeTasks, ...finishedTasks];
+    } else if (selectedFilter === 'Active') {
+      return activeTasks;
     } else {
-      const taskToMove = finishedTasks.find(task => task.id === taskId);
-      if (taskToMove) {
-        const updatedTasks = [...tasks, { ...taskToMove, originalIndex: taskToMove.originalIndex }];
-        updatedTasks.sort((a, b) => a.originalIndex - b.originalIndex);
-        setTasks(updatedTasks);
-        setFinishedTasks(finishedTasks.filter(task => task.id !== taskId));
-      }
+      return finishedTasks;
     }
   };
 
-  const renderItem = ({ item }: { item: { id: number, text: string, originalIndex: number } }) => (
-    <View style={styles.taskItem}>
-      <CheckBox
-        value={false}
-        onValueChange={() => handleCheck(item.id, true)}
-        style={styles.checkbox}
-      />
-      <Text style={styles.taskText}>{item.text}</Text>
-    </View>
-  );
-
-  const renderFinishedItem = ({ item }: { item: { id: number, text: string, originalIndex: number } }) => (
-    <View style={styles.taskItem}>
-      <CheckBox
-        value={true}
-        onValueChange={() => handleCheck(item.id, false)}
-        style={styles.checkbox}
-      />
-      <Text style={styles.finishedTaskText}>{item.text}</Text>
-    </View>
-  );
-
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar barStyle="light-content" />
+      <StatusBar barStyle="dark-content" />
 
-      {/* Header */}
-      <View style={styles.header}>
-        <Text style={styles.date}>
-          <Text style={styles.boldDay}>{currentDate.day}</Text>, {currentDate.fullDate}
-        </Text>
-        <Text style={styles.headerText}>Hi, Farah!</Text>
-        <Text style={styles.subHeaderText}>Another to-do?</Text>
+      {/* Top Menu */}
+      <View style={styles.topMenu}>
+        <TouchableOpacity style={styles.menuButton}>
+          <Text style={styles.inactiveMenuText}>Calendar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.menuButton}>
+          <Text style={styles.activeMenuText}>My Tasks</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.menuButton}>
+          <Text style={styles.inactiveMenuText}>Notes</Text>
+        </TouchableOpacity>
       </View>
 
+      {/* Task Section */}
+      <View style={styles.taskSection}>
+        <View style={styles.taskHeader}>
+          <View>
+            <Text style={styles.taskTitle}>My Tasks</Text>
+            <Text style={styles.taskDate}>{currentDate}</Text>
+          </View>
 
-      {/* Body */}
-      <View style={styles.body}>
-        <Text style={styles.bodyText}>LIST A TASK</Text>
-        <View style={styles.inputField}>
-          <TextInput
-            style={styles.input}
-            placeholder='Type your to-do here'
-            value={inputTask}
-            onChangeText={setInputTask}
-          />
-          <TouchableOpacity style={styles.addButton} onPress={addTask}>
-            <Text style={styles.addButtonText}>+</Text>
+          <TouchableOpacity style={styles.newTaskButton} onPress={() => setModalVisible(true)}>
+            <Text style={styles.newTaskButtonText}>+ New Task</Text>
           </TouchableOpacity>
         </View>
 
-        <View style={styles.tasksContainer}>
-          <Text style={styles.bodyText}>YOUR TASKS</Text>
-          <FlatList
-            data={tasks}
-            renderItem={renderItem}
-            keyExtractor={item => item.id.toString()}
-            style={styles.taskList}
-            contentContainerStyle={styles.taskListContent}
-          />
+        {/* Task Filters */}
+        <View style={styles.filterSection}>
+          <TouchableOpacity
+            style={[styles.filterButton, selectedFilter === 'All' && styles.selectedFilterButton]}
+            onPress={() => setSelectedFilter('All')}
+          >
+            <Text style={selectedFilter === 'All' ? styles.selectedFilterText : styles.filterText}>All</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={[styles.filterButton, selectedFilter === 'Active' && styles.selectedFilterButton]}
+            onPress={() => setSelectedFilter('Active')}
+          >
+            <Text style={selectedFilter === 'Active' ? styles.selectedFilterText : styles.filterText}>Active</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={[styles.filterButton, selectedFilter === 'Finished' && styles.selectedFilterButton]}
+            onPress={() => setSelectedFilter('Finished')}
+          >
+            <Text style={selectedFilter === 'Finished' ? styles.selectedFilterText : styles.filterText}>Finished</Text>
+          </TouchableOpacity>
         </View>
 
-        {finishedTasks.length > 0 && (
-          <View style={styles.finishedTasksContainer}>
-            <Text style={styles.bodyText}>FINISHED TASKS</Text>
-            <FlatList
-              data={finishedTasks}
-              renderItem={renderFinishedItem}
-              keyExtractor={item => item.id.toString()}
-              style={styles.taskList}
-              contentContainerStyle={styles.taskListContent}
-            />
-          </View>
-        )}
+        {/* Check All / Uncheck All Buttons */}
+        <View style={styles.actionButtons}>
+          <TouchableOpacity style={styles.actionButton} onPress={handleCheckAll}>
+            <Text style={styles.actionButtonText}>Check All</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.actionButton} onPress={handleUncheckAll}>
+            <Text style={styles.actionButtonText}>Uncheck All</Text>
+          </TouchableOpacity>
+        </View>
+
+        {/* Task List */}
+        <FlatList
+          data={filterTasks()}
+          renderItem={renderTaskItem}
+          keyExtractor={item => item.id.toString()}
+          style={styles.taskList}
+          contentContainerStyle={styles.taskListContent}
+        />
       </View>
+
+      {/* Modal for New Task */}
+      <NewTaskModal
+        visible={isModalVisible}
+        onClose={() => setModalVisible(false)}
+        onSubmit={handleAddTask}
+      />
     </SafeAreaView>
   );
 }
@@ -147,102 +172,73 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#FFFFFF',
   },
-  date: {
-    fontSize: 12,
-    fontWeight: 'normal',  // Normal weight for the whole date except the bold day
-    color: '#545454',
-    position: 'absolute',
-    right: 20,
-    top: 20,
-  },
-  boldDay: {
-    fontWeight: 'bold',  // Only the day of the week will be bold
-  },
-  header: {
-    backgroundColor: '#D5E7FF',
-    padding: 40,
-    paddingTop: 60,
-    paddingBottom: 80,
-  },
-  headerText: {
-    fontSize: 28,
-    fontWeight: 'bold',
-    color: '#263263',
-  },
-  subHeaderText: {
-    fontSize: 16,
-    color: '#56618E',
-    marginTop: 5,
-  },
-  body: {
-    flex: 1,
-    backgroundColor: '#fff',
-    borderTopLeftRadius: 50,
-    borderTopRightRadius: 50,
-    padding: 20,
-    marginTop: -50,
-  },
-  bodyText: {
-    fontSize: 14,
-    fontWeight: 'bold',
-    paddingTop: 15,
-    paddingLeft: 20,
-    paddingBottom: 3,
-    color: '#333',
-  },
-  inputField: {
+  topMenu: {
     flexDirection: 'row',
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-    paddingLeft: 10,
-    paddingRight: 10,
+    justifyContent: 'space-around',
+    paddingTop: 20,
+    backgroundColor: '#F8F8F8',
   },
-  input: {
-    flex: 1,
-    height: 60,
-    backgroundColor: '#F7F7F7',
-    paddingLeft: 15,
+  menuButton: {
+    paddingVertical: 10,
+  },
+  activeMenuText: {
     fontSize: 16,
-    borderTopLeftRadius: 12,
-    borderTopRightRadius: 12,
-    borderBottomLeftRadius: 0,
-    borderBottomRightRadius: 0,
+    fontWeight: 'bold',
+    color: '#000000',
   },
-  tasksContainer: {
-    flex: 1,
-    marginBottom: 25,
-  },
-  finishedTasksContainer: {
-    flex: 1,
-    marginBottom: 20,
-  },
-  taskItem: {
-    backgroundColor: '#F1F5FB',
-    padding: 15,
-    borderRadius: 10,
-    marginBottom: 10,
-    flexDirection: 'row', 
-    alignItems: 'center',
-  },
-  taskText: {
+  inactiveMenuText: {
     fontSize: 16,
-    color: '#545454',
-    paddingLeft: 5,
+    color: '#A0A0A0',
   },
-  addButton: {
-    backgroundColor: '#D9D9D9',
-    borderTopRightRadius: 10,
-    borderBottomRightRadius: 10,
-    width: 50,
-    height: 60,
-    justifyContent: 'center',
+  taskSection: {
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+  },
+  taskHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
   },
-  addButtonText: {
+  myTasksTitle: { // Style for the title "My Tasks"
     fontSize: 24,
     fontWeight: 'bold',
-    color: '#545454',
+  },
+  taskDate: {
+    fontSize: 16,
+    color: '#A0A0A0',
+    marginTop: 5,
+  },
+  newTaskButton: {
+    backgroundColor: '#E6F0FF',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+  },
+  newTaskButtonText: {
+    fontSize: 16,
+    color: '#007BFF',
+  },
+  filterSection: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 20,
+  },
+  filterButton: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+  filterText: {
+    fontSize: 16,
+    color: '#A0A0A0',
+  },
+  selectedFilterButton: {
+    borderBottomWidth: 2,
+    borderBottomColor: '#007BFF',
+  },
+  selectedFilterText: {
+    fontSize: 16,
+    color: '#007BFF',
+    fontWeight: 'bold',
   },
   taskList: {
     flexGrow: 1,
@@ -250,15 +246,66 @@ const styles = StyleSheet.create({
   taskListContent: {
     paddingHorizontal: 10,
   },
-  checkbox: {
-    padding: 0,
-    marginRight: 0,
+  taskItem: {
+    backgroundColor: '#F1F5FB',
+    padding: 15,
+    borderRadius: 10,
+    marginBottom: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
   },
-  finishedTaskText: {
+  taskDetails: {
+    paddingLeft: 10,
+  },
+  taskTitle: {
     fontSize: 16,
-    color: '#8C8C8C',
+    color: '#545454',
+  },
+  finishedTaskTitle: { // Style for finished task titles
+    fontSize: 16,
+    color: '#A0A0A0',
     textDecorationLine: 'line-through',
-  }
+  },
+  taskDescription: {
+    fontSize: 14,
+    color: '#7A7A7A',
+  },
+  taskTimestamp: {
+    fontSize: 12,
+    color: '#A0A0A0',
+    marginTop: 5,
+  },
+  activeCircle: {
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    backgroundColor: '#B0B0B0',
+  },
+  finishedCircle: {
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    backgroundColor: '#4A90E2',
+  },
+  actionButtons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginVertical: 15,
+  },
+  actionButton: {
+    backgroundColor: '#E6F0FF',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    marginHorizontal: 10,
+  },
+  actionButtonText: {
+    fontSize: 16,
+    color: '#007BFF',
+  },
 });
+
+
+
 
 export default App;
